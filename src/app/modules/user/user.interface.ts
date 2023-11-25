4081ea4eb520e76e3a03a66befe52f0ca6f0e00b
meher09 <mehernigarcu@gmail.com>
2023-11-25 18:07:50 +0600

    Added Interface for the users

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,31 @@
+import { Schema, model, connect } from 'mongoose';
+
+export type FullName = {
+    firstName: string;
+    lastName: string;
+};
+
+export type Address = {
+    street: string;
+    city: string;
+    country: string;
+};
+
+export type Order = {
+    productName: string;
+    price: number;
+    quantity: number;
+};
+
+export type User = {
+    userId: number;
+    username: string;
+    password: string;
+    fullName: FullName;
+    age: number;
+    email: string;
+    isActive: boolean;
+    hobbies: string[];
+    address: Address;
+    orders: Order[];
+}
